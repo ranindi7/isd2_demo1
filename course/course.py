8c64772691995a1646dae577ed12d221348c687e
Mike bialowas <mikebialowas@videoedit-s.academic.rrc.ca>
2024-08-26 12:50:49 -0500

    made a change

---
+++ course/course.py
@@ -0,0 +1,12 @@
+from department.department import Department
+
+class Course():
+    """
+    Represents template for a course
+    """
+    def __init__(self, name:str, department:Department, credit_hours:int):
+
+        self.__name = name
+    
+
+
