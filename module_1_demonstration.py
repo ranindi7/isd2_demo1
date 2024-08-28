9ceacd1aa69c915d68bf13c9c9078c5340761284
Mike bialowas <mikebialowas@Mikes-MBP.wp.shawcable.net>
2024-08-28 14:10:09 -0500

    This completes demo 1

--- module_1_demonstration.py
+++ module_1_demonstration.py
@@ -1,4 +1,6 @@
 # 1. Import as necessary.
+from course.course import Course
+from department.department import Department
 
 
 
@@ -6,6 +8,11 @@ def main():
     # 1. Create an instance of the Course class with valid inputs.
     #    Print the instance.
     #    If an exception occurs, print the exception instance.
+    try: 
+        intermediate_software_development = Course("Intermediate Software Developer", Department.COMPUTER_SCIENCE,6)
+        print(intermediate_software_development)
+    except ValueError as e:
+        print(e)
 
 
 
@@ -13,7 +20,11 @@ def main():
     # 2. Create an instance of the Course class with invalid inputs.
     # Print the instance.
     # If an exception occurs, print the exception instance.
-
+    try:
+        invalid_department = Course("Invalid Course", "Invalid Department",6)
+        print(invalid_department)
+    except ValueError as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
